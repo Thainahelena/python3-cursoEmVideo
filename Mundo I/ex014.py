9ab5f600f207db67958deeefca680811719d0d44
Thaina Helena <thaina.lena@gmail.com>
2021-02-24 23:16:57 -0300

    Exercicio 014

---
+++ Mundo I/ex014.py
@@ -0,0 +1,4 @@
+celsius = float(input('Digite a temperatura em graus celsius: '))
+fareihnt = ((9 * celsius) / 5) + 32
+
+print('A temperatura de {}º Celsius equivale a {}º Fareihnt!'.format(celsius,fareihnt))
