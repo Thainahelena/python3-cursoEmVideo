4cc375f0b33a9f7bede052b257fef5cd91ab419c
Thaina Helena <thaina.lena@gmail.com>
2021-02-24 23:04:43 -0300

    Exercicio 3

---
+++ Mundo I/ex003.py
@@ -0,0 +1,5 @@
+number_01 = int(input('Digite um numero: '))
+number_02 = int(input('Digite outro numero: '))
+
+soma = number_01 + number_02
+print('A soma entre {} e {} vale {}'.format(number_01,number_02,soma))
