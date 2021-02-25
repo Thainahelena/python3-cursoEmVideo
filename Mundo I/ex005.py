e4fa2cf6500bf835eee0e93c16b632cb80378467
Thaina Helena <thaina.lena@gmail.com>
2021-02-24 23:08:56 -0300

    Exercicio 005

---
+++ Mundo I/ex005.py
@@ -0,0 +1,6 @@
+numero = int(input('Digite um número inteiro: '))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print('\n Você digitou o número: {} \n Seu antecessor é: {} \n Seu sucessor é: {}'. format(numero, antecessor, sucessor))
