41f86a6d64a81c88643d9ee7583963c1fd925441
Thaina Helena <thaina.lena@gmail.com>
2021-02-24 23:16:08 -0300

    Exercicio 012

---
+++ Mundo I/ex012.py
@@ -0,0 +1,4 @@
+preco_0 = float(input('Digite o preço do produto: R$ '))
+preco_1 = preco_0 * 0.95
+
+print(' Parabéns! Você ganhou um desconto de 5%! \n Seu produto de R$ {}, sairá por R$ {:.2f}!'.format(preco_0, preco_1))
