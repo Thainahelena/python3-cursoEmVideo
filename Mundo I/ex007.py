a23f64ecaca5c35724ce615db73749e08b0aa8de
Thaina Helena <thaina.lena@gmail.com>
2021-02-24 23:09:33 -0300

    Exercicio 007

---
+++ Mundo I/ex007.py
@@ -0,0 +1,6 @@
+nota_1 = float(input('Digite sua 1ª Nota: '))
+nota_2 = float(input('Digite sua 2ª Nota: '))
+
+media = (nota_1+nota_2)/2
+
+print('\n Sua primeira nota é: {:.1f}\n Sua segunda nota é: {:.1f}\n A média entre suas notas é: {:.1f}'.format(nota_1, nota_2, media))
