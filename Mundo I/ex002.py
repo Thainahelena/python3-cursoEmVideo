86908932a4c921018e3f150d3085bccf28b8d329
Thaina Helena <thaina.lena@gmail.com>
2021-02-24 23:02:24 -0300

    Exercicio 002

---
+++ Mundo I/ex002.py
@@ -0,0 +1,7 @@
+# Várias Variáveis
+
+dia = input('Qual é o dia do seu nascimento? ')
+mes = input('Qual é o mês do seu nascimento? ')
+ano = input('Qual é o ano do seu nascimento? ')
+
+print('Você nasceu no dia {}'.format(dia), 'do {}'.format(mes), 'de {}.' .format(ano), 'Correto?')
