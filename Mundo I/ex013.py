c39beffc480f6bae56e323bcda19a4ab86a207c5
Thaina Helena <thaina.lena@gmail.com>
2021-02-24 23:16:39 -0300

    Exercicio 013

---
+++ Mundo I/ex013.py
@@ -0,0 +1,4 @@
+salario_0 = float(input('Digite o salário do funcionário: R$ '))
+salario_1 = salario_0 * 1.15
+
+print('Um funcionário que recebe um salário de R$ {:.2f}, passa a receber R$ {:.2f}, com um aumento de 15%!'.format(salario_0, salario_1))
