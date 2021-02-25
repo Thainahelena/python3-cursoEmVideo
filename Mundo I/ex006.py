a32e4a677e6e49d3a001d7e0b22605a851a73d6c
Thaina Helena <thaina.lena@gmail.com>
2021-02-24 23:09:10 -0300

    Exercicio 006

---
+++ Mundo I/ex006.py
@@ -0,0 +1,6 @@
+numero = int(input('Digite um número inteiro: '))
+
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero ** (1/2)
+print("O número que você digitou foi: {}\n Seu dobro é: {}\n Seu triplo é: {}\n Sua raíz quadrada é: {:.2f}" .format(numero,dobro, triplo, raiz))
